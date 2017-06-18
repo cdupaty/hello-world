b5a69061425e722a1287a22722f6bfcb28f83eb9
Christian Dupaty <christian.dupaty@ac-aix-marseille.fr>
2017-06-18 09:03:45 +0200

    Un nouvel ajout

--- hello.c
+++ hello.c
@@ -3,7 +3,9 @@
 int main(void) 
 {
 	printf("Salut tout le monde");
-	printf("comment allez vous bien ?")
+	printf("comment allez vous bien ?");
+	printf("it's a fake ?");
+	printf("on allonge le code");
 	return(0);
 }
 
